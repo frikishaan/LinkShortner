92a3b607621931891b71ba745e2b9e822d2c5ad1
Ishaan Sheikh <sheikh.ysbh@gmail.com>
2019-10-27 19:12:13 +0530

    Log config changes

--- config/logging.php
+++ config/logging.php
@@ -37,13 +37,13 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['daily'],
+            'channels' => ['single'],
             'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+            // 'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
